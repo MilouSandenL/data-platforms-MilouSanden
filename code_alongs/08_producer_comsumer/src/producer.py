ce0993da06fe89b19617c4fa7b5599f3e75ad144
Milou Sanden <Milou.Sanden@gmail.com>
2025-01-27 12:46:56 +0100

    producing jokes

--- code_alongs/08_producer_comsumer/src/producer.py
+++ code_alongs/08_producer_comsumer/src/producer.py
@@ -1,12 +1,25 @@
 from pathlib import Path
 import json
 from pprint import pprint
+from quixstreams import Application
 
 data_path = Path(__file__).parents[1] / "data"
 
-print(data_path)
 
 with open(data_path / "jokes.json", "r") as file:
      jokes = json.load(file)
      
 pprint(jokes)
+
+app = Application(broker_adress="localhost:9092", consumer_group="text_splitter")
+
+jokes_topic = app.topic(name= "jokes", value_serializer="json")
+# print(jokes_topic)
+
+def main():
+    with app.get_producer() as producer:
+        print(producer)
+
+# den här koden körs när man exikuverar scriptet och inte när man importerar moduler
+if __name__ == '__main__':
+    main()
