b90445d395279bc8a3f45c12e5e853c784409f8a
Milou Sanden <Milou.Sanden@gmail.com>
2025-01-27 13:05:46 +0100

    produced events kafka

--- code_alongs/08_producer_comsumer/src/producer.py
+++ code_alongs/08_producer_comsumer/src/producer.py
@@ -11,15 +11,26 @@ with open(data_path / "jokes.json", "r") as file:
      
 pprint(jokes)
 
-app = Application(broker_adress="localhost:9092", consumer_group="text_splitter")
+app = Application(broker_address="localhost:9092", consumer_group="text_splitter")
 
 jokes_topic = app.topic(name= "jokes", value_serializer="json")
 # print(jokes_topic)
 
 def main():
     with app.get_producer() as producer:
-        print(producer)
+        # print(producer)
+        
+        for joke in jokes:
+            
+            kafka_msg = jokes_topic.serialize(key = joke["joke_id"], value = joke)
+            
+            print(f"Produced message: key = {kafka_msg.key}, value = {kafka_msg.value}")
+            
+            producer.produce(
+                topic = "jokes", key = str(kafka_msg.key), value = kafka_msg.value
+            )
 
 # den här koden körs när man exikuverar scriptet och inte när man importerar moduler
 if __name__ == '__main__':
+    pprint(jokes)
     main()
