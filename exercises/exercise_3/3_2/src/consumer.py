c419c1f4fa96c7e5618d42a72297b7070edce455
Milou Sanden <Milou.Sanden@gmail.com>
2025-01-31 12:21:02 +0100

    order.json in kafka

--- exercises/exercise_3/3_2/src/consumer.py
+++ exercises/exercise_3/3_2/src/consumer.py
@@ -1,1 +1,37 @@
+# consumer tar emot meddelanden från kafka, bearbetar och skriver ut på ett läsbart sätt.
+from quixstreams import Application
 
+# Skapar en consumer som prenumererar på kafka-brokern och den specifika consumer group (som vi har i producern) för att vara i samma grupp och kunna hantera meddelanden som produceras.
+# aouto_offset_reset='earliest' används för att om consumern startar om- kommer den börja läsa från det första meddelandet i topicen.
+app = Application(broker_address="localhost:9092", consumer_group="order_group", auto_offset_reset="earliest")
+
+# skapar topic där man får alla meddelanden från producern. Gör om (deseralizerar) meddelandet från json till python-objekt.
+orders_topic = app.topic(name="orders_topic", value_deserializer="json")
+
+# skapar en dataframe som tar emot alla meddelanden från topicen order_topic. dataframe --> varje rad är ett meddelande.
+sdf = app.dataframe(topic=orders_topic)
+
+# Funktion för att bearbeta varje ordermeddelande. skriver ut information om ordern.
+def process_order(order):
+    print(f"\nInput: {order}")
+    print(f"Order ID: {order['order_id']}")
+    print(f"Kund: {order['customer']}")
+
+    total_price = 0
+    for product in order["products"]:
+        product_name = product["name"]
+        price = product["price"]
+        quantity = product["quantity"]
+        total_price += price * quantity
+        print(f"Produkt: {product_name:<20} Antal: {quantity:<5} Pris: {price:.2f}")
+
+    print(f"Total pris: {total_price:.2f}")
+
+# varje gång ett nytt meddelande (order) kommer in från kafka, så bearbetas den med funktionen process_order.
+sdf.update(lambda message: process_order(message))
+
+# Starta applikationen
+if __name__ == '__main__':
+    print("Startar Kafka consumer...")
+# app.run() används så att applikationen ska startas, vilket innebär att kafka consumer börjar prenumerera på topicen och bearbetar inkommande meddelanden.
+    app.run()
--- exercises/exercise_3/3_2/src/producer.py
+++ exercises/exercise_3/3_2/src/producer.py
@@ -1,28 +1,33 @@
+# producern skickar data (meddelanden) till kafka. 
 from pathlib import Path
 import json
 from quixstreams import Application
 from pprint import pprint
 
-# Data-mappen är i 3_1/data
+# vägen (pathen) där order.json ligger. Path(__file__) ger den nuvarande filens sökväg, .parten[1] går upp ett steg för att komma till den överordnade mappen. (data)
 data_path = Path(__file__).parents[1] / "data"
 
-# Läs in order.json
+# öppnar filen order.json och läser den med hjälp av 'r', sparar i variabel file. läser in den med ett python-objekt (orders) med hjälp av json.load()
 with open(data_path / "order.json", "r") as file:
     orders = json.load(file)
 
-# Skapar en Kafka-producent
+# Skapar en kafka applikation som skickar data till kafka-brokern på localhost:9092.
+# brokern är den server som hanterar alla dataflöden. Definierar en consumer group (används när man vill ha olika consumers som prenumererar på samma topic)
 app = Application(broker_address="localhost:9092", consumer_group="order_group")
 
-# Skapa en Kafka-topic för orders
+# definierar kafka-topic med namnet 'order_topic' - value_serializer används för att tala om att datan ska skickas som JSON.
 order_topic = app.topic(name="orders_topic", value_serializer="json")
 
-# Producerar meddelanden till Kafka
+# Funktion för producer logiken körs. 
 def main():
+# skapar en producer som kan skriva data till kafka med hjälp av app.get_producer()
     with app.get_producer() as producer:
+# loopar igenom varje order i json-filen och skapar ett meddelande som skickas till kafka.
         for order in orders:
+# skapar ett kafka-meddelande där varje order är omvandlade till ett format som kafka kan skicka. nyckeln är order_id och värder är hela orderonjektet i jsonfilen.
             kafka_msg = order_topic.serialize(key=order["order_id"], value=order)
             print(f"Producerat meddelande: key = {kafka_msg.key}, value = {kafka_msg.value}")
-            
+# skickar meddelandet till kafka med .produce() - meddelandet som är producerat skickas till topicen order_topics.
             producer.produce(
                 topic="orders_topic",
                 key=str(kafka_msg.key),
